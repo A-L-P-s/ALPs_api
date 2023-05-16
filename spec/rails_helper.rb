327f57b73822596ba3a0618b54902d0a2c92647e
Huy Phan <huyphan520@gmail.com>
2023-05-16 17:54:48 -0500

    Update VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -76,7 +76,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('UNSPLASH_API_KEY') { ENV['UNSPLASH_API_KEY'] }
-  config.filter_sensitive_data('OPEN_AI_API_KEY') { ENV['OPEN_AI_API_KEY'] }
   config.configure_rspec_metadata!
+  # config.filter_sensitive_data('UNSPLASH_API_KEY') { ENV['UNSPLASH_API_KEY'] }
+  # config.filter_sensitive_data('OPEN_AI_API_KEY') { ENV['OPEN_AI_API_KEY'] }
 end
