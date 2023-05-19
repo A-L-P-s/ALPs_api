0bb9f855eda76b0099f252571475c6131b0bc4d3
Caleb Thomas <cjthomas00@comcast.net>
2023-05-18 20:26:16 -0600

    test: write test for :update_sentences in openai_facade.

--- spec/facades/openai_facade_spec.rb
+++ spec/facades/openai_facade_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe OpenaiFacade, :vcr do
       @turkish_user = User.create(id: 55, name: "Deniz", preferred_lang: "Turkish")
 
       @tr_challenge1 = Challenge.create(id: 50, user_id: 55, language: "Turkish", verb: "(e/a) gitmek", eng_verb: "to go", image_url: "https://images.unsplash.com/photo-1500835556837-99ac94a94552?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80", image_alt_text: "Plane flying")
-      @tr_sentence1 = Sentence.create(id: 10, challenge_id: 50, grammar_point: "gelecek zaman (-ecek)", eng_grammar_point: "future tense", user_sent: "Yarın annem beni havalimanına bırakmak için araba kullanacak.", ai_sent: "Yarın annem beni havalimanına bırakmak için araba kullanacak.")
-      @tr_sentence2 = Sentence.create(id: 11, challenge_id: 50, grammar_point: "olumsuz geçmiş zaman (-me/-ma + di/-tı)", eng_grammar_point: "negative past tense", user_sent: "Dün havalimana gittik ama arkadaşım uçak gelmedi.", ai_sent: "Dün havalimanına gittik, ancak arkadaşımızın uçağı gelmedi.")
+      @tr_sentence1 = Sentence.create(id: 10, challenge_id: 50, grammar_point: "gelecek zaman (-ecek)", eng_grammar_point: "future tense", user_sent: "Yarın annem beni havalimanına bırakmak için araba kullanacak.")
+      @tr_sentence2 = Sentence.create(id: 11, challenge_id: 50, grammar_point: "olumsuz geçmiş zaman (-me/-ma + di/-tı)", eng_grammar_point: "negative past tense", user_sent: "Dün havalimana gittik ama arkadaşım uçak gelmedi.")
     end
 
     it "can :srub_response" do
@@ -17,9 +17,26 @@ RSpec.describe OpenaiFacade, :vcr do
     end
 
     xit "can :check_challenge_with_ai" do
+      
     end
 
-    xit "can :update_sentences" do
+    it "can :update_sentences" do
+      data = {:correction1=>"Bu yaz Hopa'ya gidiyorum.",
+      :explanation1=>"The sentence is already correct. It uses the correct grammar şimdiki zaman (-iyor) and the verb (i) gitmek to express an action that is happening now.",
+      :correction2=>"Her yıl biz Ankara'ya gideriz.",
+      :explanation2=>
+       "The corrected sentence uses the correct grammar geniş zaman (-ir/-er) and the verb (i) gitmek to express a habitual action that happens every year. Also, the verb ending is changed to the plural form to match the subject 'biz'."}
+        
+      expect(@tr_challenge1.sentences.first.ai_sent).to eq(nil)
+      expect(@tr_challenge1.sentences.first.ai_explanation).to eq(nil)
+      expect(@tr_challenge1.sentences.last.ai_sent).to eq(nil)
+      expect(@tr_challenge1.sentences.last.ai_explanation).to eq(nil)
+      
+      OpenaiFacade.update_sentences(@tr_challenge1, data)
+      expect(@tr_challenge1.sentences.first.ai_sent).to eq("Bu yaz Hopa'ya gidiyorum.")
+      expect(@tr_challenge1.sentences.first.ai_explanation).to eq("The sentence is already correct. It uses the correct grammar şimdiki zaman (-iyor) and the verb (i) gitmek to express an action that is happening now.")
+      expect(@tr_challenge1.sentences.second.ai_sent).to eq("Her yıl biz Ankara'ya gideriz.")
+      expect(@tr_challenge1.sentences.second.ai_explanation).to eq("The corrected sentence uses the correct grammar geniş zaman (-ir/-er) and the verb (i) gitmek to express a habitual action that happens every year. Also, the verb ending is changed to the plural form to match the subject 'biz'.")
     end
   end
 end
